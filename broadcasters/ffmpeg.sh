aa8d2e89788cd50c1fa79159294b6ca4652bc87e
kyle <truongondaihoc@gmail.com>
2022-07-07 14:20:40 +0700

    update ffmpeg command

--- broadcasters/ffmpeg.sh
+++ broadcasters/ffmpeg.sh
@@ -181,9 +181,10 @@ ffmpeg \
 	-v info \
 	-stream_loop -1 \
 	-i ${MEDIA_FILE} \
-	-map 0:a:0 \
-	-acodec libopus -ab 128k -ac 2 -ar 48000 \
+	-map 0:a:0? \
+	-acodec libopus \
 	-map 0:v:0 \
-	-pix_fmt yuv420p -c:v libvpx -b:v 1000k -deadline realtime -cpu-used 4 \
+	-pix_fmt yuv420p -c:v libvpx -b:v 1000k -deadline realtime \
+	-cpu-used 4 \
 	-f tee \
 	"[select=a:f=rtp:ssrc=${AUDIO_SSRC}:payload_type=${AUDIO_PT}]rtp://${audioTransportIp}:${audioTransportPort}?rtcpport=${audioTransportRtcpPort}|[select=v:f=rtp:ssrc=${VIDEO_SSRC}:payload_type=${VIDEO_PT}]rtp://${videoTransportIp}:${videoTransportPort}?rtcpport=${videoTransportRtcpPort}"
